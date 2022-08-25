04e7a9d8211be8353d134e360fa8284a1ffe165d
thanaratth63 <108852188+thanaratth63@users.noreply.github.com>
2022-08-25 11:03:06 +0700

    Update Midterm1-65.js

--- src/controllers/Midterm1-65.js
+++ src/controllers/Midterm1-65.js
@@ -3,7 +3,7 @@ module.exports ={
     add1 (req,res){
         input = parseInt(JSON.stringify(req.body['input']))
         console.log('input = '+ input)
-        output = input + 1
+        output = input*input
         res.send(output.toString())
     }
 
